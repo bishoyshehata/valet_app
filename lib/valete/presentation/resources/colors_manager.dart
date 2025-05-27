657f535c3a0664d8583dd9ce93f8d6e060d9630a
Bishoy Shehata <bishoyshehata9@gmail.com>
2025-05-28 01:53:29 +0300

    update color palete

--- lib/valete/presentation/resources/colors_manager.dart
+++ lib/valete/presentation/resources/colors_manager.dart
@@ -2,28 +2,14 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color secondry = const Color(0xff282828);
-  static Color primary = const Color(0xffD6DE19);
-  static Color background = const Color(0xff101010);
+  static Color primary = const Color(0xff63D8F2);
+  static Color background = const Color(0xff012340);
   static Color white = const Color(0xffFFFFFF);
   static Color lightGrey = const Color(0xffcfcfcf);
-  static Color grey = const Color(0xff606060);
-  static Color darkGrey = const Color(0xff3B3B3B);
+  static Color grey = const Color(0xff0477BF);
+  static Color darkGrey = const Color(0xff010626);
   static Color success = const Color(0xff28a745);
   static Color error = const Color(0xffdc3545);
   static Color warning = const Color(0xffe8853f);
   static Color blue = const Color(0xff18FFFF);
 }
-
-// class ColorManager {
-//   static Color secondry = const Color(0xff282828);
-//   static Color primary = const Color(0xffD98032);
-//   static Color background = const Color(0xff012340);
-//   static Color white = const Color(0xffFFFFFF);
-//   static Color lightGrey = const Color(0xffcfcfcf);
-//   static Color grey = const Color(0xff0477BF);
-//   static Color darkGrey = const Color(0xff010626);
-//   static Color success = const Color(0xff28a745);
-//   static Color error = const Color(0xffdc3545);
-//   static Color warning = const Color(0xffe8853f);
-//   static Color blue = const Color(0xff18FFFF);
-// }
